b93b16a5779da12e792fc9a0cc56003d764c44d3
Toshio Minei <sminei10@gmail.com>
2021-07-18 03:20:38 -0500

    added private routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
+import { Navbar } from './components/Navbar'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Router } from '@reach/router'
-import { Navbar } from './components/Navbar'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return (
@@ -16,6 +23,23 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth ? (
+              <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+            ) : (
+              <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>
+            )
+        }
+
+      </UserLogged>
       <Navbar />
     </div>
   )
