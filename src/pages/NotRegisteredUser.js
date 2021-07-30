370fdb0dad35f7c385a0a535a11667002039d6ff
Toshio Minei <sminei10@gmail.com>
2021-07-30 17:58:57 -0500

    save the auth tokens in the session storage

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,47 +1,44 @@
-import React from 'react'
-import Context from '../Context'
+import React, { useContext } from 'react'
+import { Context } from '../Context'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
 import { LoginMutation } from '../container/LoginMutation'
 
-export const NotRegisteredUser = () => (
-  <Context.Consumer>
-    {
-      ({ activateAuth }) => {
-        return (
-          <>
-            <RegisterMutation>
-              {
-                (register, { _, loading, error }) => {
-                  const onSubmit = async ({ email, password }) => {
-                    const input = { email, password }
-                    const variables = { input }
-                    await register({ variables })
-                    activateAuth()
-                  }
-                  const errorMsg = error && 'User already exists or there is a problem'
+export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context)
+  return (
+    <>
+      <RegisterMutation>
+        {
+          (register, { _, loading, error }) => {
+            const onSubmit = async ({ email, password }) => {
+              const input = { email, password }
+              const variables = { input }
+              const { data } = await register({ variables })
+              const { signup } = data
+              activateAuth(signup)
+            }
+            const errorMsg = error && 'User already exists or there is a problem'
 
-                  return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title='Sign Up' />
-                }
-              }
-            </RegisterMutation>
-            <LoginMutation>
-              {
-                (login, { _, loading, error }) => {
-                  const onSubmit = async ({ email, password }) => {
-                    const input = { email, password }
-                    const variables = { input }
-                    await login({ variables })
-                    activateAuth()
-                  }
-                  const errorMsg = error && 'Email & password incorrect or user doesn\'t exist'
-                  return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title='Sign In' />
-                }
-              }
-            </LoginMutation>
-          </>
-        )
-      }
-    }
-  </Context.Consumer>
-)
+            return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title='Sign Up' />
+          }
+        }
+      </RegisterMutation>
+      <LoginMutation>
+        {
+          (login, { _, loading, error }) => {
+            const onSubmit = async ({ email, password }) => {
+              const input = { email, password }
+              const variables = { input }
+              const { data } = await login({ variables })
+              const { login: loginToken } = data
+              activateAuth(loginToken)
+            }
+            const errorMsg = error && 'Email & password incorrect or user doesn\'t exist'
+            return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title='Sign In' />
+          }
+        }
+      </LoginMutation>
+    </>
+  )
+}
