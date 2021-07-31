76a9495a23629cb0b508b780299dcca2eed45057
Toshio Minei <sminei10@gmail.com>
2021-07-31 12:43:02 -0500

    add a 404 page and refactor routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { Navbar } from './components/Navbar'
@@ -6,37 +6,30 @@ import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
+import { NotFound } from './pages/NotFound'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
-import { Router } from '@reach/router'
-import Context from './Context'
+import { Redirect, Router } from '@reach/router'
+import { Context } from './Context'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
     <div>
       <GlobalStyle />
       <Logo />
       <div style={{ padding: '0 10px' }}>
         <Router>
+          <NotFound default />
           <Home path='/' />
           <Home path='/pet/:categoryId' />
           <Detail path='/detail/:detailId' />
+          {!isAuth && <NotRegisteredUser path='/login' />}
+          {!isAuth && <Redirect from='/favs' to='/login' />}
+          {!isAuth && <Redirect from='/user' to='/login' />}
+          {isAuth && <Redirect from='/login' to='/' />}
+          <Favs path='/favs' />
+          <User path='/user' />
         </Router>
-        <Context.Consumer>
-          {
-            ({ isAuth }) =>
-              isAuth ? (
-                <Router>
-                  <Favs path='/favs' />
-                  <User path='/user' />
-                </Router>
-              ) : (
-                <Router>
-                  <NotRegisteredUser path='/favs' />
-                  <NotRegisteredUser path='/user' />
-                </Router>
-              )
-          }
-        </Context.Consumer>
       </div>
       <Navbar />
     </div>
