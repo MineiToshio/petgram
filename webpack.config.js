271ca19ee601ca8d0b122b86010f168766d5eb18
Toshio Minei <sminei10@gmail.com>
2021-08-01 16:13:06 -0500

    added a manifest file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const path = require('path')
 
 module.exports = {
   output: {
@@ -8,6 +10,18 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Your pet photo app',
+      shortname: 'Petgram 🐶',
+      description: 'Petgram lets you find pretty pet photos',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [{
+        src: path.resolve('src/assets/icon.png'),
+        sizes: [96, 128, 192, 256, 384, 512],
+        purpose: 'maskable'
+      }]
     })
   ],
   module: {
